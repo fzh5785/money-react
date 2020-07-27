b788d616c622f1d64fdc2afd512b4943d5f8a2e3
ISol <599304776@qq.com>
2020-07-27 15:36:14 +0800

    导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,87 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+   height: 100vh;
+   display: flex;
+   flex-direction: column;
+`;
+const Main = styled.div`
+   flex-grow: 1;
+   overflow: auto;
+`;
+const Nav = styled.nav`
+   border: 1px solid red;
+   >ul {
+    display: flex;
+    >li {
+    width: 33.333%;
+    text-align: center;
+    padding: 16px;
+}
+}
+`;
 
 function App() {
   return (
-    <div >
-      hello
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags/>
+            </Route>
+            <Route path="/money">
+              <Money/>
+            </Route>
+            <Route path="/statistics">
+              <Statistics/>
+            </Route>
+            <Redirect exact from="/" to='/money'/>
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签</Link>
+            </li>
+            <li>
+              <Link to="/money">记账</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
+    </Router>
   );
+}
+
+function NoMatch() {
+  return <h2>404</h2>;
+}
+
+function Statistics() {
+  return <h2>Statistics</h2>;
+}
+
+function Tags() {
+  return <h2>Tags</h2>;
+}
+
+function Money() {
+  return <h2>Money</h2>;
 }
 
 export default App;
