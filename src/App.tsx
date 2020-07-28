9d09edf6e2fd6becd83dddac0e2c26272d5a2806
ISol <599304776@qq.com>
2020-07-28 14:59:50 +0800

    创建 views 目录

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import {
-  HashRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from 'react-router-dom';
-import Layout from './components/Layout';
+import {HashRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import Money from './views/Money';
+import Statistics from './views/Statistics';
+import Tags from './views/Tags';
+import NoMatch from './views/NoMatch';
 
 
 function App() {
@@ -27,34 +25,6 @@ function App() {
         </Route>
       </Switch>
     </Router>
-  );
-}
-
-function NoMatch() {
-  return <h2>404</h2>;
-}
-
-function Statistics() {
-  return (
-    <Layout>
-      <h2>statistics</h2>
-    </Layout>
-  );
-}
-
-function Tags() {
-  return (
-    <Layout>
-      <h2>tags</h2>
-    </Layout>
-  );
-}
-
-function Money() {
-  return (
-    <Layout>
-      <h2>money</h2>
-    </Layout>
   );
 }
 
