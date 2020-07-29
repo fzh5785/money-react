9a930e19cd198159a22c0c7f1827658c48b61d15
ISol <599304776@qq.com>
2020-07-29 14:30:48 +0800

    完成 Money.jsx 的 UI

--- src/views/Money.jsx
+++ src/views/Money.jsx
@@ -2,10 +2,19 @@ import Layout from "../components/Layout"
 import React from "react"
 import styled from "styled-components"
 
+const MyLayout = styled(Layout)`
+  display: flex;
+  flex-direction: column;
+`
 
 const TagsSection = styled.section`
   background:#fff;
   padding: 12px 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
   >ol {
     margin: 0 -12px;
     >li {
@@ -66,14 +75,71 @@ const CategorySection = styled.section`
     }
   }
 `
-const NumberPadSection = styled.section`
 
+const NumberPadSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  > .output {
+    background:#fff;
+    font-size: 36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: 0 -5px 5px -5px rgba(0,0,0,0.25),
+                0  5px 5px -5px rgba(0,0,0,0.25);
+  }
+  > .pad {
+
+    >button{
+      float: left;
+      font-size: 18px;
+      border: none;
+      width: 25%;
+      height: 64px;
+      &.ok{
+        height: 128px;
+        float: right;
+      }
+      &.zero {
+        width: 50%;
+      }
+
+
+        &:nth-child(1) {
+          background: #f2f2f2;
+        }
+
+        &:nth-child(2), &:nth-child(5) {
+          background: #e0e0e0
+        }
+
+        &:nth-child(3), &:nth-child(6), &:nth-child(9) {
+          background: #d3d3d3
+        }
+
+        &:nth-child(4), &:nth-child(7), &:nth-child(10) {
+          background: #c1c1c1
+        }
+
+        &:nth-child(8), &:nth-child(11), &:nth-child(13) {
+          background: #b8b8b8
+        }
+
+        &:nth-child(14) {
+          background: #a9a9a9
+        }
+
+        &:nth-child(12) {
+          background: #9a9a9a
+        }
+    }
+  }
 `
 
 
 function Money() {
   return (
-    <Layout>
+    <MyLayout>
       <TagsSection>
         <ol>
           <li>衣</li>
@@ -99,8 +165,8 @@ function Money() {
       </CategorySection>
 
       <NumberPadSection>
-        <div>100</div>
-        <div>
+        <div className='output'>100</div>
+        <div className='pad clearfix'>
           <button>1</button>
           <button>2</button>
           <button>3</button>
@@ -112,13 +178,13 @@ function Money() {
           <button>7</button>
           <button>8</button>
           <button>9</button>
-          <button>OK</button>
-          <button>0</button>
+          <button className='ok'>OK</button>
+          <button className='zero'>0</button>
           <button>.</button>
         </div>
       </NumberPadSection>
 
-    </Layout>
+    </MyLayout>
   )
 }
 
