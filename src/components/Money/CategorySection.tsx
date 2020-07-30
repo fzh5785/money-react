4654b2eebfecbdabbab6485707ed3e9a42405a5b
ISol <599304776@qq.com>
2020-07-30 14:13:20 +0800

    完成 category 切换功能

--- src/components/Money/CategorySection.tsx
+++ src/components/Money/CategorySection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const CategorySection = styled.section`
+const Wrapper = styled.section`
   font-size: 24px;
   >ul {
     display: flex;
@@ -22,5 +23,25 @@ const CategorySection = styled.section`
       }
     }
   }
-`
-export {CategorySection}
+`;
+const CategorySection: React.FC = () => {
+  const [category, setCategory] = useState('-');
+  const [categoryList] = useState<('-' | '+')[]>(['-', '+']);
+  const categoryMap = {
+    '-': '支出',
+    '+': '收入'
+  };
+  return (
+    <Wrapper>
+      <ul>
+        {categoryList.map(c =>
+          <li className={category === c ? 'selected' : ''}
+              onClick={() => {setCategory(c);}}
+          >{categoryMap[c]}
+          </li>
+        )}
+      </ul>
+    </Wrapper>
+  );
+};
+export {CategorySection};
--- src/views/Money.jsx
+++ src/views/Money.jsx
@@ -16,13 +16,8 @@ function Money() {
     <MyLayout>
       <TagsSection/>
       <NoteSection/>
+      <CategorySection/>
 
-      <CategorySection>
-        <ul>
-          <li className='selected'>支出</li>
-          <li>收入</li>
-        </ul>
-      </CategorySection>
 
       <NumberPadSection>
         <div className='output'>100</div>
