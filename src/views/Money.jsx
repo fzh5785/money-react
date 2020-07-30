8f12742e50dc738648265cf8465b089532ff1439
ISol <599304776@qq.com>
2020-07-30 15:10:45 +0800

    完成 NumberPad 基本功能

--- src/views/Money.jsx
+++ src/views/Money.jsx
@@ -4,7 +4,8 @@ import styled from "styled-components"
 import {TagsSection} from "../components/Money/TagsSection"
 import {NoteSection} from "../components/Money/NoteSection"
 import {CategorySection} from "../components/Money/CategorySection"
-import {NumberPadSection} from "../components/Money/NumberPadSection"
+import {NumberPadSection} from "../components/Money/NumberSection"
+
 
 const MyLayout = styled(Layout)`
   display: flex;
@@ -17,28 +18,7 @@ function Money() {
       <TagsSection/>
       <NoteSection/>
       <CategorySection/>
-
-
-      <NumberPadSection>
-        <div className='output'>100</div>
-        <div className='pad clearfix'>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>删除</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>清空</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button className='ok'>OK</button>
-          <button className='zero'>0</button>
-          <button>.</button>
-        </div>
-      </NumberPadSection>
-
+      <NumberPadSection/>
     </MyLayout>
   )
 }
