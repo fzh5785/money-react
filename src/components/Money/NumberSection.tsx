13151813b1bc2eddd4026f4192b91642215d8049
ISol <599304776@qq.com>
2020-07-30 15:30:16 +0800

    重构 NumberPad

--- src/components/Money/NumberSection.tsx
+++ src/components/Money/NumberSection.tsx
@@ -1,65 +1,8 @@
-import styled from 'styled-components';
 import React, {useState} from 'react';
-
-const Wrapper = styled.section`
-      display: flex;
-  flex-direction: column;
-  > .output {
-    background:#fff;
-    font-size: 36px;
-    line-height: 72px;
-    text-align: right;
-    padding: 0 16px;
-    box-shadow: 0 -5px 5px -5px rgba(0,0,0,0.25),
-                0  5px 5px -5px rgba(0,0,0,0.25);
-  }
-  > .pad {
-
-    >button{
-      float: left;
-      font-size: 18px;
-      border: none;
-      width: 25%;
-      height: 64px;
-      &.ok{
-        height: 128px;
-        float: right;
-      }
-      &.zero {
-        width: 50%;
-      }
+import {Wrapper} from  './NumberPadSection/Wrapper'
+import {computerOutput} from './NumberPadSection/computerOutput';
 
 
-        &:nth-child(1) {
-          background: #f2f2f2;
-        }
-
-        &:nth-child(2), &:nth-child(5) {
-          background: #e0e0e0
-        }
-
-        &:nth-child(3), &:nth-child(6), &:nth-child(9) {
-          background: #d3d3d3
-        }
-
-        &:nth-child(4), &:nth-child(7), &:nth-child(10) {
-          background: #c1c1c1
-        }
-
-        &:nth-child(8), &:nth-child(11), &:nth-child(13) {
-          background: #b8b8b8
-        }
-
-        &:nth-child(14) {
-          background: #a9a9a9
-        }
-
-        &:nth-child(12) {
-          background: #9a9a9a
-        }
-    }
-  }
-`;
 
 
 const NumberPadSection: React.FC = () => {
@@ -75,41 +18,8 @@ const NumberPadSection: React.FC = () => {
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
     if (text === null) {return;}
-    switch (text) {
-      case '0' :
-      case '1' :
-      case '2' :
-      case '3' :
-      case '4' :
-      case '5' :
-      case '6' :
-      case '7' :
-      case '8' :
-      case '9' :
-        if (output === '0') {
-          setOutput(text);
-        } else {
-          setOutput(output + text);
-        }
-        break;
-      case '.':
-        if (output.indexOf('.') >= 0) {return;}
-        setOutput(output + '.');
-        break;
-      case '删除':
-        if (output.length === 1) {
-          setOutput('');
-        } else {
-          setOutput(output.slice(0, -1));
-        }
-        break;
-      case '清空':
-        setOutput('');
-        break;
-      case 'OK':
-        console.log('ok');
-        break;
-    }
+    if(text === 'OK'){return;}
+    setOutput(computerOutput(text,output))
   };
   return (
     <Wrapper>
