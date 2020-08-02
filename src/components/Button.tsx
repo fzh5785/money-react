e21250ced0b83a39057dcb07b05e0bdc6a811b33
ISol <599304776@qq.com>
2020-08-02 11:50:35 +0800

    封装 Button 组件

---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+const Button = styled.button`
+  font-size: 18px;
+  color: white;
+  background:#f60;
+  padding: 8px 12px;
+  border: none;
+  border-radius: 4px;
+`;
+
+export {Button}
